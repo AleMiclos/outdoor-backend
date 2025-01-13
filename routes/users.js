23c270b8540f6bd96cc2ce23c852ad2ba417c0db
AleMiclos <alexandremiclos@gmail.com>
2025-01-13 19:28:21 -0300

    18:26 13/01r

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,15 @@ const express = require('express');
 const router = express.Router();
 
 // Exemplo de rota
-router.get('/', (req, res) => {
-  res.send('Rota de usuários funcionando!');
+router.get('/', async (req, res) => {
+  try {
+    const users = await users.find(); // Certifique-se de que `User` é seu modelo
+    res.json(users); // Certifique-se de retornar um array
+  } catch (error) {
+    console.error('Erro ao obter usuários:', error);
+    res.status(500).json({ message: 'Erro no servidor' });
+  }
 });
+
 
 module.exports = router;
