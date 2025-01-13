cefb452a48745ec5e83d0e1c869e611aa154e477
AleMiclos <alexandremiclos@gmail.com>
2025-01-13 13:52:42 -0300

    rota register

--- models/User.js
+++ models/User.js
@@ -1,24 +1,13 @@
 const mongoose = require('mongoose');
 
-const UserSchema = new mongoose.Schema({
-  name: { type: String, required: true }, // Novo campo de nome obrigatório
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  role: { type: String, enum: ['admin', 'cliente'], required: true },
-  totemId: { 
-    type: mongoose.Schema.Types.ObjectId, 
-    ref: 'Totem',
-    validate: {
-      validator: function(value) {
-        if (this.role === 'cliente' && !value) {
-          return false; // Cliente sem totemId não é válido
-        }
-        return true;
-      },
-      message: 'Clientes devem ter um totemId.'
-    }
-  },
+  role: { type: String, required: true },
+  totemId: { type: String, default: null }, // Permite que totemId seja nulo
 });
+
 
 const User = mongoose.model('User', UserSchema);
 module.exports = User;
