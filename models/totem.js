b72022b702df93b978da3b3f42f9430a30f8c388
AleMiclos <alexandremiclos@gmail.com>
2025-01-23 23:09:02 -0300

    Atualização endereço2

--- models/totem.js
+++ models/totem.js
@@ -4,10 +4,8 @@ const totemSchema = new mongoose.Schema({
   title: { type: String, required: true },
   description: { type: String, required: true },
   videoUrl: { type: String, required: true },
-  status: { type: String, enum: ["online", "offline"], default: "offline" },
-  address: { type: String, required: true }, // Novo campo para endereço do totem
+  isActive: { type: Boolean, default: true },
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
 });
 
 module.exports = mongoose.model('Totem', totemSchema);
-
