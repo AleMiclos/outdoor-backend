03c443f038c656120bcbeb9e615c7d6a2932b494
AleMiclos <159150312+AleMiclos@users.noreply.github.com>
2025-04-10 09:50:37 -0300

    Update server.js novo link

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(morgan('combined'));
 
 // Configuração do CORS
 const corsOptions = {
-  origin: ["http://localhost:4200", "https://alldoor.vercel.app", "https://alldoorteste.vercel.app", "https://alldoortestes.vercel.app"],
+  origin: ["http://localhost:4200", "https://alldoor.vercel.app", "https://alldoorteste.vercel.app", "https://alldoortestes.vercel.app", "https://alldoortest.vercel.app"],
   methods: "GET,POST,DELETE,PUT",
   allowedHeaders: "Content-Type,Authorization",
   credentials: true
